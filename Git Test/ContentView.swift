4fbf020bb6e294887eb58a95ed53a4c6521922df
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2024-09-09 16:23:36 +0800

    Made the list items bold :)

--- Git Test/ContentView.swift
+++ Git Test/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     var body: some View {
         ForEach(listy, id:\.self){listable in
             Text(listable)
+                .bold
+                                  
     }
 }
 
