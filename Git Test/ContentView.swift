c0789e682514aa42a4dee2a536593f7388d892ea
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2024-09-09 16:42:18 +0800

    Alert

--- Git Test/ContentView.swift
+++ Git Test/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var listy = ["salkjsa", "saxuj"]
+    @State var alertShown = false
     var body: some View {
-        List{
-            ForEach(listy, id:\.self){listable in
-                Text("\(listable)")
-                    .bold()
+        Button{
+            alertShown.toggle()
+        }label:{
+            Text("Show alert!")
+        }
+        .alert(Text("Title"), isPresented: $alertShown) {
+            Button(role: .destructive){
+                print("The Show alert button was pressed")
+            }label:{
+                Text("Delete")
             }
-            
+        }message:{
+            Text("Are you sure you want to delete this account? This action cannot be undone.")
         }
     }
 }
