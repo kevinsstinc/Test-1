87eedccb21f534be84e12ac52c87b0dda030d355
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2024-09-09 15:59:02 +0800

    Initial Commit

---
+++ Git Test/Git_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TestApp.swift
+//  Git Test
+//
+//  Created by Joseph Kevin Fredric on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
